dc7d277a285a0dc3a866dc2631bbe2ba1ae61716
thedon122 <donovan122@yahoo.com>
2021-02-06 17:55:05 -0500

    finshed the setup

---
+++ SP500-stock-price.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import yfinance as yf
+
+st.title('S&P 500 App')
+
+st.markdown("""
+This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+""")
