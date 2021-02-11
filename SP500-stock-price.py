2e50632c10ca4d3091c058ba7c990f099fecebf3
thedon122 <donovan122@yahoo.com>
2021-02-10 23:41:27 -0500

    fix plot of closing price

--- SP500-stock-price.py
+++ SP500-stock-price.py
@@ -65,3 +65,8 @@ def price_plot(symbol):
   plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
   plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
   plt.xticks(rotation=90)
+  plt.title(symbol, fontweight='bold')
+  plt.xlabel('Date', fontweight='bold')
+  plt.ylabel('Closing Price', fontweight='bold')
+  return st.pyplot()
+
