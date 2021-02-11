bf5e0f924979ae63ec7f70fb1603d4ef89e51e8c
thedon122 <donovan122@yahoo.com>
2021-02-10 23:42:21 -0500

    added symbol to plot closing

--- SP500-stock-price.py
+++ SP500-stock-price.py
@@ -70,3 +70,9 @@ def price_plot(symbol):
   plt.ylabel('Closing Price', fontweight='bold')
   return st.pyplot()
 
+num_company = st.sidebar.slider('Number of Companies', 1, 5)
+
+if st.button('Show Plots'):
+    st.header('Stock Closing Price')
+    for i in list(df_selected_sector.Symbol)[:num_company]:
+        price_plot(i)
